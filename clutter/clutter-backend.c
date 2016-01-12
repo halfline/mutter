77ebd1ce7705df49c2d421cd0ce88a30c1cf1bc3
Ray Strode <rstrode@redhat.com>
2016-01-12 12:13:52 -0500

    wayland: drop wayland backend

--- clutter/clutter-backend.c
+++ clutter/clutter-backend.c
@@ -76,9 +76,6 @@
 #ifdef CLUTTER_WINDOWING_EGL
 #include "egl/clutter-backend-eglnative.h"
 #endif
-#ifdef CLUTTER_INPUT_WAYLAND
-#include "wayland/clutter-device-manager-wayland.h"
-#endif
 
 #ifdef CLUTTER_HAS_WAYLAND_COMPOSITOR_SUPPORT
 #include <cogl/cogl-wayland-server.h>
@@ -502,11 +499,6 @@ _clutter_create_backend (void)
     retval = g_object_new (CLUTTER_TYPE_BACKEND_X11, NULL);
   else
 #endif
-#ifdef CLUTTER_WINDOWING_WAYLAND
-  if (backend == NULL || backend == I_(CLUTTER_WINDOWING_WAYLAND))
-    retval = g_object_new (CLUTTER_TYPE_BACKEND_WAYLAND, NULL);
-  else
-#endif
 #ifdef CLUTTER_WINDOWING_EGL
   if (backend == NULL || backend == I_(CLUTTER_WINDOWING_EGL))
     retval = g_object_new (CLUTTER_TYPE_BACKEND_EGL_NATIVE, NULL);
@@ -556,14 +548,6 @@ clutter_backend_real_init_events (ClutterBackend *backend)
       strcmp (input_backend, CLUTTER_INPUT_TSLIB) == 0)
     {
       _clutter_events_tslib_init (backend);
-    }
-  else
-#endif
-#ifdef CLUTTER_INPUT_WAYLAND
-  if (clutter_check_windowing_backend (CLUTTER_WINDOWING_WAYLAND) &&
-      (input_backend == NULL || input_backend == I_(CLUTTER_INPUT_WAYLAND)))
-    {
-      _clutter_events_wayland_init (backend);
     }
   else
 #endif
