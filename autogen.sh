8f6bfe117f1458a9e7f7c7dc8a4f8c17246678f1
Ray Strode <rstrode@redhat.com>
2016-01-12 11:38:07 -0500

    tests: drop tests and examples

--- autogen.sh
+++ autogen.sh
@@ -20,12 +20,6 @@ test $TEST_TYPE $FILE || {
 # https://bugzilla.gnome.org/show_bug.cgi?id=661128
 touch -t 200001010000 po/clutter-1.0.pot
 
-GTKDOCIZE=`which gtkdocize`
-if test -z $GTKDOCIZE; then
-        echo "*** No GTK-Doc found, please install it ***"
-        exit 1
-fi
-
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
         echo "*** No autoreconf found, please install it ***"
@@ -42,7 +36,6 @@ fi
 
 rm -rf autom4te.cache
 
-gtkdocize || exit $?
 autoreconf --force --install --verbose || exit $?
 
 cd "$olddir"
